c2cdcd67c77b3b7b1ca5d4946af90edd306a81bd
Dmitry Selivanov <selivanov.dmitriy@gmail.com>
2016-01-11 19:12:27 +0300

    use helper functions

--- R-pkg/src/r_wrappers.cpp
+++ R-pkg/src/r_wrappers.cpp
@@ -18,18 +18,18 @@ MSG get_msg_meta(RawVector msg_ser) {
 //' @export
 // [[Rcpp::export]]
 Int32 R_ArtmCreateMasterComponent(RawVector master_component_config) {
-//   MSG msg = get_msg_meta(master_component_config);
-//   return( ArtmCreateMasterComponent(msg.byte_size,  msg.ptr ) );
-  const char * ptr = (char *)&(*master_component_config.begin());
-  return( ArtmCreateMasterComponent(master_component_config.size(),  ptr ) );
+  MSG msg = get_msg_meta(master_component_config);
+  return( ArtmCreateMasterComponent(msg.byte_size,  msg.ptr ) );
+//   const char * ptr = (char *)&(*master_component_config.begin());
+//   return( ArtmCreateMasterComponent(master_component_config.size(),  ptr ) );
 }
 
 //' @export
 // [[Rcpp::export]]
 Int32 R_ArtmCreateDictionary(Int32 master_id, RawVector dict_component_config) {
-//   MSG msg = get_msg_meta(dict_component_config);
-//   return( ArtmCreateDictionary(master_id, msg.byte_size,  msg.ptr ) );
-  const char * ptr = (char *)&(*dict_component_config.begin());
-  return( ArtmCreateDictionary(master_id, dict_component_config.size(),  ptr ) );
+  MSG msg = get_msg_meta(dict_component_config);
+  return( ArtmCreateDictionary(master_id, msg.byte_size,  msg.ptr ) );
+//   const char * ptr = (char *)&(*dict_component_config.begin());
+//   return( ArtmCreateDictionary(master_id, dict_component_config.size(),  ptr ) );
 }
 
